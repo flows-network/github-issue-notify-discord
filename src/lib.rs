23d0c1571cd329c8e6a3ef689248988bb02c2072
Ji Chen <jaykchen@gmail.com>
2023-05-11 08:37:00 -0400

    formatted to new protocol (#2)
    * if successive events after comment in one sequence,
    message is sent for comment action only, other actions are not reported.
    
    * load var from env
    
    * no action on deleted or closed events

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
 use discord_flows::create_text_message_in_channel;
-use github_flows::{listen_to_event, EventPayload,
-    octocrab::models::events::payload::IssuesEventAction
+use github_flows::{
+    listen_to_event,
+    octocrab::models::events::payload::{IssueCommentEventAction, IssuesEventAction},
+    EventPayload,
+    GithubLogin::Provided,
 };
+
+use dotenv::dotenv;
+use std::env;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
-    let login = "jaykchen";
-    let owner = "jaykchen";
-    let repo = "a-test";
+    dotenv().ok();
+    let github_login = env::var("github_login").unwrap_or("alabulei1".to_string());
+    let github_owner = env::var("github_owner").unwrap_or("alabulei1".to_string());
+    let github_repo = env::var("github_repo").unwrap_or("a-test".to_string());
     let label_watch_list = vec![
         "good first issue".to_string(),
         "help wanted".to_string(),
@@ -16,25 +23,21 @@ pub async fn run() {
         "OSPP".to_string(),
         "Hacktoberfest".to_string(),
     ];
-    let guild_name = "myserver";
-    let channel_name = "general";
 
     listen_to_event(
-        login,
-        owner,
-        repo,
+        &Provided(github_login),
+        &github_owner,
+        &github_repo,
         vec!["issues", "issue_comment"],
-        |payload| handler(guild_name, channel_name, payload, &label_watch_list),
+        |payload| handler(&label_watch_list, payload),
     )
     .await;
 }
 
-async fn handler(
-    guild_name: &str,
-    channel_name: &str,
-    payload: EventPayload,
-    label_watch_list: &Vec<String>,
-) {
+async fn handler(label_watch_list: &Vec<String>, payload: EventPayload) {
+    let discord_server = env::var("discord_server").unwrap_or("Vivian Hu's server".to_string());
+    let discord_channel = env::var("discord_channel").unwrap_or("general".to_string());
+
     let lowercase_list = label_watch_list
         .into_iter()
         .map(|word| word.to_ascii_lowercase())
@@ -55,7 +58,8 @@ async fn handler(
                 let label_name = label.name;
                 if lowercase_list.contains(&label_name.to_lowercase()) {
                     let body = format!("{label_name}: {issue_title} by {user}\n{issue_url}");
-                    create_text_message_in_channel(guild_name, channel_name, body, None);
+                    create_text_message_in_channel(&discord_server, &discord_channel, body, None);
+
 
                     return;
                 }
@@ -63,6 +67,12 @@ async fn handler(
         }
 
         EventPayload::IssueCommentEvent(e) => {
+            if e.action == IssueCommentEventAction::Deleted
+                || e.action == IssueCommentEventAction::Edited
+            {
+                return;
+            }
+
             let issue = e.issue;
             let comment = e.comment;
             let issue_title = issue.title;
@@ -76,7 +86,7 @@ async fn handler(
                     let body = format!(
                             "A new comment is added for {issue_title}: {comment_content}\n{comment_url}"
                         );
-                    create_text_message_in_channel(guild_name, channel_name, body, None);
+                    create_text_message_in_channel(&discord_server, &discord_channel, body, None);
 
                     return;
                 }
